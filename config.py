8612eaf10b8e12fccc17619cdc6b04681fa639ac
coreylam <coreylam@163.com>
2023-08-15 21:47:21 +0800

    update by fast_commit

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
-GITHUB_TOKEN = ''
+# github 个人令牌
+GITHUB_TOKEN = ""
+
+# gitee 个人令牌
 GITEE_TOKEN = ""
-GITHUB_OWNER = "coreylam"
-GITEE_OWNER = "coreylam"
+
+# 需要同步的仓库名单，为空是默认获取全部仓库
 REPO_LIST = [
-    "blogs"
+]
+
+# 不同步的仓库名单，名单中的仓库不会被同步
+# 特别的，如果仓库名称在 gitee 中不存在，也不会同步
+EXCLUDED_REPO_LIST = [
 ]
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from github import Github
 from github import Auth
 
 
-def get_commit_id(owner, repo_name, access_token, web_site='github'):
+def get_git_client(access_token, web_site="github"):
     # using an access token
     auth = Auth.Token(access_token)
 
@@ -23,9 +23,13 @@ def get_commit_id(owner, repo_name, access_token, web_site='github'):
         base_url = "https://api.github.com"
 
     g = Github(auth=auth, base_url=base_url)
+    return g
+   
+
+def get_commit_id(git_client, owner, repo_name):
 
     # 获取仓库
-    repo = g.get_repo(f"{owner}/{repo_name}")
+    repo = git_client.get_repo(f"{owner}/{repo_name}")
 
     # 获取仓库的历史提交记录
     commits = repo.get_commits()
@@ -54,12 +58,13 @@ def sync_repo(src_url, dest_url):
         repo.git.push("--tags")
         print("推送完成")
 
-def github2gitee(github_access_token, gitee_access_token, 
+def github2gitee(github_client, gitee_client,
                  github_owner, gitee_owner,
                  github_repo_name, gitee_repo_name):
     # 判断是否最新，若非最新，则更新 gitee 仓库
-    gitee_commit_id = get_commit_id(gitee_owner, gitee_repo_name, gitee_access_token, web_site='gitee')
-    github_commit_id = get_commit_id(github_owner, github_repo_name, github_access_token)
+    github_commit_id = get_commit_id(github_client, github_owner, github_repo_name)
+
+    gitee_commit_id = get_commit_id(gitee_client, gitee_owner, gitee_repo_name)
 
     if github_commit_id != gitee_commit_id:
         print(f"仓库 {github_owner}/{github_repo_name} 最新 commit id 为 {github_commit_id}, 更新中")
@@ -67,7 +72,7 @@ def github2gitee(github_access_token, gitee_access_token,
         github_repo_url = f"git@github.com:{github_owner}/{github_repo_name}.git"
         gitee_repo_url = f"git@gitee.com:{gitee_owner}/{gitee_repo_name}.git"
         sync_repo(github_repo_url, gitee_repo_url)
-        gitee_commit_id = get_commit_id(gitee_owner, gitee_repo_name, gitee_access_token, web_site='gitee')
+        gitee_commit_id = get_commit_id(gitee_client, gitee_owner, gitee_repo_name)
     
     if gitee_commit_id == github_commit_id:
         print(f"仓库 {gitee_owner}/{gitee_repo_name} 已更新到最新。 \ncommit id: {github_commit_id}")
@@ -75,17 +80,39 @@ def github2gitee(github_access_token, gitee_access_token,
         print("更新失败，请人工检查。\ncommit id: {github_commit_id}")
 
 
+def get_repo_names(git_client):
+    """ 获取指定用户的仓库列表
+    """
+    user = git_client.get_user()
+    repos = user.get_repos()
+    return [i.name for i in repos]
+
 def run():
     # load config
 
     # 定义要获取提交历史的仓库信息
     cnt = 1
-    for repo_name in config.REPO_LIST:
+    github_client = get_git_client(config.GITHUB_TOKEN, 'github')
+    github_owner = github_client.get_user().login
+
+    gitee_client = get_git_client(config.GITEE_TOKEN, 'gitee')
+    gitee_owner = gitee_client.get_user().login
+
+    gitee_repo_names = get_repo_names(github_client)
+
+    if config.REPO_LIST:
+        all_repo_names = config.REPO_LIST
+    else:
+        all_repo_names = get_repo_names(github_client)
+
+    scan_repo_names = [i for i in all_repo_names if i not in config.EXCLUDED_REPO_LIST and i in gitee_repo_names]
+
+    for repo_name in scan_repo_names:
         if cnt > 1:
             print("")
-        print(f"{cnt}/{len(config.REPO_LIST)} 开始扫描仓库 : {config.GITEE_OWNER}/{repo_name}")
-        github2gitee(config.GITHUB_TOKEN, config.GITEE_TOKEN,
-                     config.GITHUB_OWNER, config.GITEE_OWNER,
+        print(f"{cnt}/{len(scan_repo_names)} 开始扫描仓库 : {github_owner}/{repo_name}")
+        github2gitee(github_client, gitee_client,
+                     github_owner, gitee_owner,
                      repo_name, repo_name)
         cnt += 1   
 
