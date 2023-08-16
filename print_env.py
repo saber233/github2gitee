a9a6fbd0aa3c376e9b03e5e9f68b5f41aa0176c9
coreylam <coreylam@163.com>
2023-08-16 10:12:48 +0800

    update by fast_commit

---
+++ print_env.py
@@ -0,0 +1,17 @@
+import os
+
+def get_all_env_variables():
+    """
+    获取并打印所有环境变量
+
+    :return: None
+    """
+    # 使用os.environ获取所有的环境变量
+    env_variables = os.environ
+
+    # 遍历所有环境变量并打印
+    for key, value in env_variables.items():
+        print(f"{key}: {value}")
+
+get_all_env_variables()
+
